fbdb13b5b7783a4e3dcfd855c1e83257be0a0982
SoyEtoor <torreshectormig@gmail.com>
2024-04-12 17:14:02 +0000

    token

--- controllers/index.js
+++ controllers/index.js
@@ -14,12 +14,15 @@ const createUser = async (req, res) => {
 const getAllUsers = async (req, res) => {
 	console.log('getting users');
 	try {
+		console.log(1)
 		const users = await models.Rick.findAll({
 			include: [
 			]
 		});
+		console.log (2)
 		return res.status(200).json({ users});
 	} catch (error) {
+		console.log(error)
 		return res.status(500).send(error.message);
 	}
 };
